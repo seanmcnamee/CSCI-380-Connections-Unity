921fe0200a52881b0fc89ef9bea92858ca240693
Sean McNamee <seanmcnamee.45@gmail.com>
2020-12-01 23:36:24 -0500

    update unity code

---
+++ Assets/GameManager.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public string username;
+
+    public int maxMessage = 25;
+
+    public GameObject chatPanel, textObject;
+    public InputField chatBox;
+
+    public Color playerMessage, info;
+
+
+    [SerializeField]
+    List<Message> messageList = new List<Message>();
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (chatBox.text != "")
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                SendMessageToChat(username + ": " + chatBox.text, Message.MessageType.playerMessage);
+                chatBox.text = "";
+            }
+        }
+        else
+        {
+            if (!chatBox.isFocused && Input.GetKeyDown(KeyCode.Return))
+            {
+                chatBox.ActivateInputField();
+            }
+        }
+
+        /*
+        if (!chatBox.isFocused)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                SendMessageToChat("SPACE Bar", Message.MessageType.info);
+            }
+        }
+        */
+    }
+
+    public void SendMessageToChat(string text, Message.MessageType messageType)
+    {
+        if (messageList.Count >= maxMessage)
+        {
+            Destroy(messageList[0].textObject.gameObject);
+            messageList.Remove(messageList[0]);
+        }
+        Message newMessage = new Message();
+
+        newMessage.text = text;
+
+        GameObject newText = Instantiate(textObject, chatPanel.transform);
+
+        newMessage.textObject = newText.GetComponent<Text>();
+
+        newMessage.textObject.text = newMessage.text;
+        newMessage.textObject.color = MessageTypeColor(messageType);
+
+        messageList.Add(newMessage);
+    }
+
+    Color MessageTypeColor(Message.MessageType messageType)
+    {
+        Color color = info;
+
+        switch (messageType)
+        {
+            case Message.MessageType.playerMessage:
+                color = playerMessage;
+                break;
+            case Message.MessageType.info:
+                color = info;
+                break;
+            default:
+                break;
+        }
+
+        return color;
+    }
+
+
+}
+
+
+[System.Serializable]
+public class Message
+{
+    public string text;
+    public Text textObject;
+    public MessageType messageType;
+
+    public enum MessageType
+    {
+        playerMessage,
+        info
+    }
+}
---
+++ Assets/Scripts/SceneSwitching.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitching : MonoBehaviour
+{
+    [SerializeField]
+    private string menuSwitch = null;
+
+    public void SwitchToMenu()
+    {
+        SceneManager.LoadScene(menuSwitch);
+    }
+}
